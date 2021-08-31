3cf1c755f2c9aa979a95e7a62120ef406cb3a971
MarcoDeRosso <mcoder@hotmail.it>
2021-08-31 11:50:36 +0200

    js function for genre

--- js/script.js
+++ js/script.js
@@ -4,13 +4,23 @@ new Vue(
     {
         el: '#app',
         data: {
-            discs: []
+            discs: [],
+            genreList:[]
         },
         created() {
             axios.get('http://localhost/php-ajax-dischi/backend/api.php')
                 .then((response) => {
                     this.discs = response.data;
             })   
+        },
+        methods:{
+            genreArray: function(){
+                this.discs.forEach((element)=>{
+                  if(!this.genreList.includes(element.genre)){
+                    this.genreList.push(element.genre)
+                  }
+                })
+              },
         }
 
     }
